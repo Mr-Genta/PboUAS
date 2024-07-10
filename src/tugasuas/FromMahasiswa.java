049473a2b3e9158bf31dd75d4c7e4e1f61980aa3
Mr-Genta <andreanardinata2@gmail.com>
2024-07-10 16:07:55 +0700

    ulang 5

--- src/tugasuas/FromMahasiswa.java
+++ src/tugasuas/FromMahasiswa.java
@@ -287,8 +287,26 @@ private void tidak_bisa_isi (){
     CboJenjang.setEnabled(false);
     CboJurusan.setEnabled(false);
 }
+//---------------------------isi-dibawah-ini------------------------------------
 
 
+//----------------------------isi-diatas-ini------------------------------------
+private void simpan() {
+    try {
+        pernyataan.executeUpdate("insert into tabelmahasiswa values"
+                               + "("+" "+TxtID.getText()+"',"
+                               + ""+""+TxtNama.getText()+"',"
+                               + ""+""+CboJenjang.getSelectedItem()+","
+                               + ""+""+CboJurusan.getSelectedItem()+"',"
+                               + ""+" "+TxtTelp.getText()+"')");
+    } 
+    catch (Exception e) {
+        JOptionPane.showMessageDialog (null, "Keterangan Error: "+e);
+    }
+    isi_table();
+    tidak_bisa_isi();
+    TblSimpan.setEnabled(false);
+}
     private void TblSimpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TblSimpanActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_TblSimpanActionPerformed
